66e9093121e34655b45bc11552ff536adcac6b4c
Dennis Lorson <dlorson@biocartis.com>
2016-03-09 12:10:12 +0100

    Modify watch script

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -9,8 +9,7 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 mkdir -p dist  # in case it doesn't exist; watchify needs it
 
 # Start watchers
-watchify -v -t [ babelify --ignore src/lib ] src/test/{*.js,**/*.js} --debug -o dist/tests.js &
-watchify -v -t [ babelify --ignore src/lib ] src/main/pileup.js      --debug -o dist/pileup.js --standalone pileup &
+watchify -v --poll -t [ babelify --ignore src/lib ] src/main/pileup.js      --debug -o dist/pileup.js --standalone pileup &
 
 # Wait until background processes end
 wait
